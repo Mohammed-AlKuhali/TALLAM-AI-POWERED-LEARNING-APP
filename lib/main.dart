845454c4dfbe6ac6006fe1941dac57797f7dd967
Hagwan Mohammed <1181302549@student.mmu.edu.my>
2023-06-26 16:42:05 +0800

    change to flutter_inappwebview

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-void main() {
-  runApp(
-     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const WebViewApp(),
-    ),
-  );
-}
-
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+class MyChromeSafariBrowser extends ChromeSafariBrowser {
+  @override
+  void onOpened() {
+    print("ChromeSafari browser opened");
+  }
 
   @override
-  State<WebViewApp> createState() => _WebViewAppState();
+  void onCompletedInitialLoad() {
+    print("ChromeSafari browser initial load completed");
+  }
+
+  @override
+  void onClosed() {
+    print("ChromeSafari browser closed");
+  }
 }
 
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  
+
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  final ChromeSafariBrowser browser = new MyChromeSafariBrowser();
+
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   void initState() {
+    widget.browser.addMenuItem(new ChromeSafariBrowserMenuItem(
+        id: 1,
+        label: 'Custom item menu 1',
+        action: (url, title) {
+          print('Custom item menu 1 clicked!');
+        }));
     super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://hagwan.github.io/'),
-      );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AR WebView'),
+        title: const Text('ChromeSafariBrowser Example'),
       ),
-      body: WebViewWidget(
-        controller: controller,
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              await widget.browser.open(
+                  url: Uri.parse("https://hagwan.github.io/"),
+                  options: ChromeSafariBrowserClassOptions(
+                      android: AndroidChromeCustomTabsOptions(
+                        shareState: CustomTabsShareState.SHARE_STATE_ON,
+                      ),
+                      ios: IOSSafariOptions(barCollapsingEnabled: true)));            },
+            child: const Text("Open Chrome Safari Browser")),
       ),
     );
   }
